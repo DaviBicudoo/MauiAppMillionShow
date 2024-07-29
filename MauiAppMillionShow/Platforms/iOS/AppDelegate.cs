f6f640251268975c380b97089eacf3348d82393f
Usuario <Usuario@CPD03-M13>
2024-07-29 15:28:52 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMillionShow/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppMillionShow
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
