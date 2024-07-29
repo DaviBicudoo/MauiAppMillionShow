10856066ea7e2c6934d42c66c83b3eced645da15
Usuario <Usuario@CPD03-M13>
2024-07-29 15:51:44 -0300

    First 5 easy questions

--- MauiAppMillionShow/App.xaml.cs
+++ MauiAppMillionShow/App.xaml.cs
@@ -1,7 +1,73 @@
-﻿namespace MauiAppMillionShow
+﻿using MauiAppMillionShow.Models;
+
+namespace MauiAppMillionShow
 {
     public partial class App : Application
     {
+        List<Question> easyQuestions = new()
+        {
+            new Question
+            {
+                Id = 1,
+                Statement = "In which Brazilian state was presenter Xuxa born?",
+                Alternatives = new()
+                {
+                    new Alternative {Correct = false,  Description = "Rio de Janeiro"},
+                    new Alternative {Correct = true,  Description = "Rio Grande do Sul"},
+                    new Alternative {Correct = false,  Description = "Santa Catarina"},
+                    new Alternative {Correct = false,  Description = "Goiás"}
+                }
+            },
+            new Question
+            {
+                Id = 2,
+                Statement = "What is the name given to the state of water in the form of ice?",
+                Alternatives = new()
+                {
+                    new Alternative {Correct = false,  Description = "Liquid"},
+                    new Alternative {Correct = true,  Description = "Solid"},
+                    new Alternative {Correct = false,  Description = "Gaseous"},
+                    new Alternative {Correct = false,  Description = "Steamy"}
+                }
+            },
+            new Question
+            {
+                Id = 3,
+                Statement = "What was singer Elis Regina's nickname?",
+                Alternatives = new()
+                {
+                    new Alternative {Correct = false,  Description = "Gauchinha"},
+                    new Alternative {Correct = false,  Description = "Paulistinha"},
+                    new Alternative {Correct = true,  Description = "Pimentinha"},
+                    new Alternative {Correct = false,  Description = "Andorinha"}
+                }
+            },
+            new Question
+            {
+                Id = 4,
+                Statement = "Who is Mickey's girlfriend?",
+                Alternatives = new()
+                {
+                    new Alternative {Correct = false,  Description = "Margarida"},
+                    new Alternative {Correct = true,  Description = "Minnie"},
+                    new Alternative {Correct = false,  Description = "The little mermaid"},
+                    new Alternative {Correct = false,  Description = "Olivia Palito"}
+                }
+            },
+            new Question
+            {
+                Id = 5,
+                Statement = "Which Brazilian folklore character has only one leg?",
+                Alternatives = new()
+                {
+                    new Alternative {Correct = false,  Description = "Cuca"},
+                    new Alternative {Correct = false,  Description = "Negrinho do pastoreio"},
+                    new Alternative {Correct = false,  Description = "Boitatá"},
+                    new Alternative {Correct = true,  Description = "Saci-Pererê"}
+                }
+            },
+        };
+
         public App()
         {
             InitializeComponent();
